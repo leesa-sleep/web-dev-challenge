5464d959ba001162baf57603b6647af1558379a7
Mord Maman <jaime.mord.maman@gmail.com>
2017-07-19 14:30:39 +0100

    adds if statement for empy search submission and clears the text section on next press of go button

--- scripts.js
+++ scripts.js
@@ -3,9 +3,16 @@ $(document).ready(function(){
    var getRecipe = function(){
 
         var recipe = $('#term').val();
+        $('#status').text("");
         console.log(recipe);
 
+        if(recipe ==''){
+            $('#status').text("Please enter a search term. Help us help you!")
+        }
+
    };
+
+
 
    $('#search').click(getRecipe);
              
