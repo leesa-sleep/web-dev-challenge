47ae75768c7fadd7f88e7a5fb934a25da11be8c7
Mord Maman <jaime.mord.maman@gmail.com>
2017-07-20 22:20:11 +0100

    adds the styling

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
-
-$(document).ready(function(){
-
+$(document).ready(function () {
 
     var objectA = {}
     var fav = []
     var fav = JSON.parse(localStorage.faves);
+    var cont;
 
     // ADD ITEM TO FAVOURITES
-    $(document).delegate( ".add", "click", function() {
+    $(document).delegate(".add", "click", function () {
         console.log(objectA)
         var num = this.id;
         console.log(num);
@@ -17,9 +16,8 @@ $(document).ready(function(){
         console.log(fav)
     });
 
-
     // REMOVES ITEM FROM FAVOURITES
-    $(document).delegate( ".remove", "click", function() {
+    $(document).delegate(".remove", "click", function () {
         console.log(fav)
         var f = this.id;
         console.log(f);
@@ -48,9 +46,9 @@ $(document).ready(function(){
 
             //API CALL
             $.getJSON(finalUrl, function (json) {
-                console.log(json.results);
+                // console.log(json.results);
                 objectA = json.results;
-                console.log(objectA);
+                // console.log(objectA);
                 console.log($.isEmptyObject(json.results));
 
                 //IN CASE NOTHING COMES BACK
@@ -75,8 +73,8 @@ $(document).ready(function(){
                         }
 
                         html += '<div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 recipe">';
-                        html += '<div class="thumbCont"><img class="thumb" src="' + thumbnail + '"><button class="add" type="button" id="' + [i] + '">+</button></div>';
-                        html += '<p><a href="' + link + '">' + title + '</a></p>';
+                        html += '<div class="thumbCont"><img class="thumb" src="' + thumbnail + '"><button class="btn btn-default add" type="button" id="' + [i] + '">+</button></div>';
+                        html += '<h4><a class="title" href="' + link + '" target="_blank">' + title + '</a></h4>';
                         html += '<p> Number of ingredients: ' + ingredients + '</p>';
                         html += '</div>';
                     }
@@ -92,60 +90,61 @@ $(document).ready(function(){
             });
         }
 
-    }//closes getRecipe();
+    }//CLOSES GET RECIPE;
 
-     //FAVES POPULATION FUNCTION
-   var listFaves = function () {
-       var myArray = JSON.parse(localStorage.getItem('faves'));
-       console.log(myArray);
-       console.log("clicked on favourites")
+    //FAVES POPULATION FUNCTION
+    var listFaves = function () {
+        var myArray = JSON.parse(localStorage.getItem('faves'));
+        console.log(myArray);
+        console.log("clicked on favourites")
 
-       var displayFaves = function () {
-           //IN CASE NOTHING COMES BACK
-           if (myArray.length === 0) {
-               console.log("failed");
-               $('#favStatus').text("Looks like there's nothing here, try searching for some delicious recipes and adding them here!")
-           }
+        var displayFaves = function () {
+            //IN CASE NOTHING COMES BACK
+            if (myArray.length === 0) {
+                console.log("failed");
+                $('#favStatus').text("Looks like there's nothing here, try searching for some delicious recipes and adding them here!")
+            }
 
-           else {
-               $('#favStatus').text("Here are your favourites! You can share with your friends or remove from the list")
-               var html = ""
-               for (var i in myArray) {
-                   var ing = myArray[i].ingredients.split(",");
-                   var title = myArray[i].title;
-                   var ingredients = ing.length;
-                   var thumbnail = myArray[i].thumbnail;
-                   var link = myArray[i].href;
+            else {
+                $('#favStatus').text("Here are your favourites! You can share with your friends or remove from the list")
+                var html = ""
+                for (var i in myArray) {
+                    var ing = myArray[i].ingredients.split(",");
+                    var title = myArray[i].title;
+                    var ingredients = ing.length;
+                    var thumbnail = myArray[i].thumbnail;
+                    var link = myArray[i].href;
 
-                   if (myArray[i].thumbnail === '') {
-                       thumbnail = 'img/food.jpg';
-                   }
+                    if (myArray[i].thumbnail === '') {
+                        thumbnail = 'img/food.jpg';
+                    }
 
-                   html += '<div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 recipe">';
-                   html += '<div class="thumbCont"><img class="thumb" src="' + thumbnail + '"><button class="remove" type="button" id="' + [i] + '">-</button></div>';
-                   html += '<p><a href="' + link + '">' + title + '</a></p>';
-                   html += '<p> Number of ingredients: ' + ingredients + '</p>';
-                   html += '<div id="share-buttons"><a href="http://www.facebook.com/sharer.php?u=' + link + '" target="_blank"><img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" /></a><a href="https://twitter.com/share?url=' + link + '&amp;text=' + title + '" target="_blank"><img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" /></a><a href="mailto:?Subject=' + title + '&amp;Body=I%20saw%20this%20and%20thought%20of%20you!%20 ' + title + '"><img src="https://simplesharebuttons.com/images/somacro/email.png" alt="Email" /></a></div>';
-                   html += '</div>';
-               }
-               $('#faveDisplay').html(html);
-           }
-       }
-       console.log("hello");
-       displayFaves();
-   }
-   
+                    html += '<div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 recipe">';
+                    html += '<div class="thumbCont"><img class="thumb" src="' + thumbnail + '"><button class="btn btn-default remove" type="button" id="' + [i] + '">-</button></div>';
+                    html += '<h4><a class="title" href="' + link + '" target="_blank">' + title + '</a></h4>';
+                    html += '<p> Number of ingredients: ' + ingredients + '</p>';
+                    html += '<div id="share-buttons"><a href="http://www.facebook.com/sharer.php?u=' + link + '" target="_blank"><img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" /></a><a href="https://twitter.com/share?url=' + link + '&amp;text=' + title + '" target="_blank"><img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" /></a><a href="mailto:?Subject=' + title + '&amp;Body=I%20saw%20this%20and%20thought%20of%20you!%20 ' + title + '"><img src="https://simplesharebuttons.com/images/somacro/email.png" alt="Email" /></a></div>';
+                    html += '</div>';
+                }
+                $('#faveDisplay').html(html);
+            }
+        }
+        console.log("hello");
+        displayFaves();
+    }//CLOSES LISTFAVES
 
-   $('#search').click(getRecipe);
+    //RUNS GET RECIPE ON SEARCH BUTTON CLICK
+    $('#search').click(getRecipe);
 
-   $('#term').keyup(function(event){
-       if(event.keyCode == 13){
-           getRecipe();
-       }
-   });
+    //RUNS GET RECIPE ON ENTER BUTTON PRESS
+    $('#term').keyup(function (event) {
+        if (event.keyCode == 13) {
+            getRecipe();
+        }
+    });
 
 
-   $('#favouritesLink').ready(listFaves);
-   
+    $('#favouritesLink').ready(listFaves);
+
 
 });
