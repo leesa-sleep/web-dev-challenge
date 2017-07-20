eed790f860d7c0079c346d21f61448fb7ab5ebe4
Mord Maman <jaime.mord.maman@gmail.com>
2017-07-20 12:58:22 +0100

    Removes an item from local storage and reloads page

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
-
-
 var fav = []
 $(document).ready(function(){
+
+
     var objectA = {}
     var fav = JSON.parse(localStorage.faves);
+
     $(document).delegate( ".add", "click", function() {
         console.log(objectA)
         var num = this.id;
@@ -12,6 +13,22 @@ $(document).ready(function(){
         fav.push(objectA[num])
         localStorage.setItem("faves", JSON.stringify(fav));
         console.log(fav)
+    });
+
+    $(document).delegate( ".remove", "click", function() {
+        console.log(fav)
+        var f = this.id;
+        console.log(f);
+        console.log(fav[f]);
+        fav.splice(f, 1);
+        localStorage.setItem("faves", JSON.stringify(fav));
+        location.reload();
+        // var num = this.id;
+        // console.log(num);
+        // console.log(objectA[num])
+        // fav.push(objectA[num])
+        // localStorage.setItem("faves", JSON.stringify(fav));
+        // console.log(fav)
     });
 
 
@@ -84,24 +101,30 @@ $(document).ready(function(){
             var displayFaves = function(){
             console.log("displaying faves");
             console.log(myArray[0].title);
+            var html=""
              for(var i in myArray){
                  console.log("words" + i);
                  console.log(myArray[i].title);
-                        // var ing = json.results[i].ingredients.split(",");
-                        // var title = json.results[i].title;
-                        // var ingredients = ing.length;
-                        // var thumbnail = json.results[i].thumbnail;
-                        // var link = json.results[i].href;
+                 console.log(myArray[i].href);
+                 console.log(myArray[i].ingredients);
+                 console.log(myArray[i].href);
 
-                        // if(json.results[i].thumbnail === ''){
-                        //     thumbnail = 'img/food.jpg';}
+                        var ing = myArray[i].ingredients.split(",");
+                        var title = myArray[i].title;
+                        var ingredients = ing.length;
+                        var thumbnail = myArray[i].thumbnail;
+                        var link = myArray[i].href;
 
-                        // html += '<div class="col-sm-4 recipe">';
-                        // html += '<div class="thumbCont"><img class="thumb" src="' +thumbnail+ '"><button class="add" type="button" id="'+[i]+'">+</button></div>';
-                        // html += '<p><a href="'+link+'">' + title +'</a></p>';
-                        // html += '<p> Number of ingredients: ' + ingredients + '</p>';
-                        // html += '</div>';
+                        if(myArray[i].thumbnail === ''){
+                            thumbnail = 'img/food.jpg';}
+
+                        html += '<div class="col-sm-4 recipe">';
+                        html += '<div class="thumbCont"><img class="thumb" src="' +thumbnail+ '"><button class="remove" type="button" id="'+[i]+'">-</button></div>';
+                        html += '<p><a href="'+link+'">' + title +'</a></p>';
+                        html += '<p> Number of ingredients: ' + ingredients + '</p>';
+                        html += '</div>';
                     }
+                    $('#faveDisplay').html(html);
         }  
         console.log("hello");
         displayFaves();
